bd41bf03bdf4c90fbd0d1325ea1d397c6e474728
ashudamn <ashutoshkushwah2014@gmail.com>
2024-09-28 20:15:26 +0530

    added reduc code, container to be added

---
+++ hooks-projects/redux/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div>
+      <h1>Home component</h1>
+      <div className="cart-wrapper"></div>
+      <div>
+        <div className="btn-wrapper">
+            <button onClick={}></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ hooks-projects/redux/src/services/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import cardItemsReducer from "./reducer";
+
+export default combineReducers({cardItemsReducer,});
